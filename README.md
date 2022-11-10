6b099147ba8c79ff563a824b9d9dfffa93d4fc05
Julian Dax <julian.dax@posteo.de>
2022-11-10 21:30:25 +0100

    start adding parser

---
+++ README.md
@@ -0,0 +1,19 @@
+# Friendly Vector Format
+
+## Values
+
+## 1. Ergonomic
+VL is easy to read and to write FVF using a text editor. 
+
+## 2. Simple to implement
+It is simple to implement renderers and parsers for FVF.
+
+## 3. Compatible
+FVF is convertable to SVG and to common raster formats.
+
+
+## TODO:
+
+1. Get Syntax highlighting to work with helix -> https://tree-sitter.github.io/tree-sitter/syntax-highlighting
+2. Render using egui
+3. Simple LSP Server
---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use tower_lsp::jsonrpc::Result;
+use tower_lsp::lsp_types::*;
+use tower_lsp::{Client, LanguageServer, LspService, Server};
+
+use tree_sitter::{Parser, Language};
+
+#[derive(Debug)]
+struct Backend {
+    client: Client,
+    parser: Parser
+}
+
+#[tower_lsp::async_trait]
+impl LanguageServer for Backend {
+    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+        Ok(InitializeResult {
+            capabilities: ServerCapabilities {
+                hover_provider: Some(HoverProviderCapability::Simple(true)),
+                completion_provider: Some(CompletionOptions::default()),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+    }
+
+    async fn initialized(&mut self, _: InitializedParams) {
+        self.parser = Parser::new();
+        extern "C" { fn tree_sitter_friendly_vector_format() -> Language; }
+        let language = unsafe { tree_sitter_friendly_vector_format() };
+        parser.set_language(language).unwrap();
+        self.client
+            .log_message(MessageType::INFO, "server initialized!")
+            .await;
+    }
+
+    async fn shutdown(&self) -> Result<()> {
+        Ok(())
+    }
+
+
+    async fn did_open(&self, params: DidOpenTextDocumentParams) {
+        let _ = params;
+
+    }
+
+    async fn completion(&self, _: CompletionParams) -> Result<Option<CompletionResponse>> {
+        Ok(Some(CompletionResponse::Array(vec![
+            CompletionItem::new_simple("Hello".to_string(), "Some detail".to_string()),
+            CompletionItem::new_simple("Bye".to_string(), "More detail".to_string())
+        ])))
+    }
+
+    async fn hover(&self, _: HoverParams) -> Result<Option<Hover>> {
+        Ok(Some(Hover {
+            contents: HoverContents::Scalar(
+                MarkedString::String("You're hovering!".to_string())
+            ),
+            range: None
+        }))
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let stdin = tokio::io::stdin();
+    let stdout = tokio::io::stdout();
+
+    let (service, socket) = LspService::new(|client| Backend { client });
+    Server::new(stdin, stdout, socket).serve(service).await;
+}
